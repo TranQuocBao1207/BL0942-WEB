790e0bcda1b9c356f8d9b0fb42d0509c3c188959
TranQuocBao1207 <tranquocbao.colette@gmail.com>
2026-05-06 20:34:39 +0700

    update code

--- monitor/urls.py
+++ monitor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import receive_data
+from .views import receive_data, latest_data
 
 urlpatterns = [
     path('api/data/', receive_data),
+    path('api/latest/', latest_data),
 ]
